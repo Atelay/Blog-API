1d1c9c03ba4ad5ce27dcf8f87372524d926edc3a
atelay <deadroll95@gmail.com>
2024-05-23 11:17:04 +0300

    update model relations

--- src/tags/models.py
+++ src/tags/models.py
@@ -8,12 +8,16 @@ class Tag(Base):
     __tablename__ = "tags"
     name: Mapped[str]
     posts: Mapped[list["Post"]] = relationship(
-        secondary="post_tags", back_populates="tags"
+        secondary="post_tags", back_populates="tags", cascade="all, delete"
     )
 
 
 class PostTag(Base):
     __tablename__ = "post_tags"
 
-    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), primary_key=True)
-    tag_id: Mapped[int] = mapped_column(ForeignKey("tags.id"), primary_key=True)
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
+    )
+    tag_id: Mapped[int] = mapped_column(
+        ForeignKey("tags.id", ondelete="CASCADE"), primary_key=True
+    )
